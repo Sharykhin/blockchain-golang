f47c5a80b7185f6d18b6bd41ed3b0b81a713671e
Siarhie Sharykhin <siarhei.sharykhin@gmail.com>
2018-01-12 09:06:23 +0300

    Lesson 6

--- utils.go
+++ utils.go
@@ -6,6 +6,7 @@ import (
 	"log"
 )
 
+// IntToHex converts an int64 to a byte array
 func IntToHex(num int64) []byte {
 	buff := new(bytes.Buffer)
 	err := binary.Write(buff, binary.BigEndian, num)
--- wallet.go
+++ wallet.go
@@ -1,33 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"log"
 	"crypto/sha256"
+	"log"
+
 	"golang.org/x/crypto/ripemd160"
-	"bytes"
 )
 
 const version = byte(0x00)
-const addressChecksumLen = 4
 const walletFile = "wallet.dat"
+const addressChecksumLen = 4
 
+// Wallet stores private and public keys
 type Wallet struct {
 	PrivateKey ecdsa.PrivateKey
-	PublicKey []byte
+	PublicKey  []byte
 }
 
-
+// NewWallet creates and returns a Wallet
 func NewWallet() *Wallet {
 	private, public := newKeyPair()
 	wallet := Wallet{private, public}
+
 	return &wallet
 }
 
+// GetAddress returns wallet address
 func (w Wallet) GetAddress() []byte {
 	pubKeyHash := HashPubKey(w.PublicKey)
+
 	versionedPayload := append([]byte{version}, pubKeyHash...)
 	checksum := checksum(versionedPayload)
 
@@ -37,6 +42,7 @@ func (w Wallet) GetAddress() []byte {
 	return address
 }
 
+// HashPubKey hashes public key
 func HashPubKey(pubKey []byte) []byte {
 	publicSHA256 := sha256.Sum256(pubKey)
 
@@ -46,9 +52,11 @@ func HashPubKey(pubKey []byte) []byte {
 		log.Panic(err)
 	}
 	publicRIPEMD160 := RIPEMD160Hasher.Sum(nil)
+
 	return publicRIPEMD160
 }
 
+// ValidateAddress check if address if valid
 func ValidateAddress(address string) bool {
 	pubKeyHash := Base58Decode([]byte(address))
 	actualChecksum := pubKeyHash[len(pubKeyHash)-addressChecksumLen:]
@@ -59,9 +67,11 @@ func ValidateAddress(address string) bool {
 	return bytes.Compare(actualChecksum, targetChecksum) == 0
 }
 
+// Checksum generates a checksum for a public key
 func checksum(payload []byte) []byte {
 	firstSHA := sha256.Sum256(payload)
 	secondSHA := sha256.Sum256(firstSHA[:])
+
 	return secondSHA[:addressChecksumLen]
 }
 
